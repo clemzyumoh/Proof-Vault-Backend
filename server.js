52c40e00ceeba4590d6f34387bba67a782392cd6
clemzyumoh <142527861+clemzyumoh@users.noreply.github.com>
2025-05-06 21:15:01 -0700

    update

--- server.js
+++ server.js
@@ -1,44 +1,4 @@
-// // server.js
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
 
-// // Load environment variables
-// dotenv.config();
-
-// // Connect to MongoDB
-// connectDB();
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// const authRoutes = require("./routes/authRoutes");
-
-// app.use("/api/auth", authRoutes);
-
-// const documentRoutes = require("./routes/documentRoutes");
-// app.use("/api/docs", documentRoutes);
-
-// const authRoutes = require("./routes/authRoutes");
-// app.use("/api/auth", authRoutes);
-
-
-
-// app.use(require("./middleware/errorHandler"));
-
-// // Routes (we'll add these next)
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
-
-// // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-// server.js
 //const express = require("express");
 import express from "express";
 //const dotenv = require("dotenv");
@@ -62,7 +22,7 @@ app.use(
   cors({
     allowedHeaders: ["Content-Type", "Authorization", "x-wallet-address"],
     // Allow custom
-    origin: "https://proof-vault.vercel.app/", // replace with actual domain
+    origin: "https://proof-vault.vercel.app", // replace with actual domain
     credentials: true,
   })
 );
